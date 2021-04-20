c426cd960f3ce0f74213f6392241f68d7f6d1527
entwwwicklung <luka.siljak@gmail.com>
2021-04-20 14:39:23 +0200

    fixed indentation

--- json_comparer.py
+++ json_comparer.py
@@ -10,30 +10,30 @@ def comparer(first_json, second_json, response=''):
         e.g. When just comparing two dicts:
         comparer(json1, json2)
     """
-	for k, v in second_json.items():
-		try:
-			first_json[k]
-		except KeyError:
-			print("{}['{}'] --- Key missing in 1st JSON".format(response,k))
-		if isinstance(v,list):
-			for y in range(len(v)):
-				try:
-					first_json[k][y]
-				except IndexError:
-					print("{}['{}'][{}] --- Object is missing from list in 1st JSON".format(response,k,y))
+    for k, v in second_json.items():
+        try:
+            first_json[k]
+        except KeyError:
+            print("{}['{}'] --- Key missing in 1st JSON".format(response,k))
+        if isinstance(v,list):
+            for y in range(len(v)):
+                try:
+                    first_json[k][y]
+                except IndexError:
+                    print("{}['{}'][{}] --- Object is missing from list in 1st JSON".format(response,k,y))
 
-	for k, v in first_json.items():
-		try:
-			if v != second_json[k]:
-				if isinstance(v, (str, int)) or v == None:
-					print("{}['{}'] --- Value is different".format(response,k))
-				elif isinstance(v, dict):
-					comparer(v, second_json[k], response=response+"['{}']".format(k))
-				elif isinstance(v, list):
-					for y in range(len(v)):
-						try:
-							comparer(v[y], second_json[k][y], response=response+"['{}'][{}]".format(k,y))
-						except IndexError:
-							print("{}['{}'][{}] --- Object is missing from list in 2nd JSON".format(response,k,y))
-		except KeyError:
-			print("{}['{}'] --- Key missing in 2nd JSON".format(response,k))
+    for k, v in first_json.items():
+        try:
+            if v != second_json[k]:
+                if isinstance(v, (str, int)) or v == None:
+                    print("{}['{}'] --- Value is different".format(response,k))
+                elif isinstance(v, dict):
+                    comparer(v, second_json[k], response=response+"['{}']".format(k))
+                elif isinstance(v, list):
+                    for y in range(len(v)):
+                        try:
+                            comparer(v[y], second_json[k][y], response=response+"['{}'][{}]".format(k,y))
+                        except IndexError:
+                            print("{}['{}'][{}] --- Object is missing from list in 2nd JSON".format(response,k,y))
+        except KeyError:
+            print("{}['{}'] --- Key missing in 2nd JSON".format(response,k))
